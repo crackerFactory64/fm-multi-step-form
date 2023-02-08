23885e9a2cd1d11333cbc2f724ee8e160c2d7b47
crackerFactory <lee.mander@gmail.com>
2023-02-08 10:38:49 +0000

    pagination styling and functionality

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,2 +1,20 @@
 import React from "react";
-export default function Pagination() {}
+export default function Pagination({ number, title, currentPage }) {
+  return (
+    <li>
+      <span
+        className={
+          number === currentPage
+            ? "pagination__number selected"
+            : "pagination__number"
+        }
+      >
+        {number}
+      </span>
+      <div className="pagination__label">
+        <p>Step {number}</p>
+        <p>{title}</p>
+      </div>
+    </li>
+  );
+}
